73a3d3b9a744cba3ff868c67f0995ebdb743ae19
jithendraathipatla <athipatala.jithendra@gmail.com>
2022-01-04 12:28:32 +0530

    first commit

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,4 @@
 var path = require('path');
-var userName = process.env['USERPROFILE'].split(path.sep)[2];
+var userName = process.env['USERPROFILE'];
 var loginId = path.join("domainName",userName);
 console.log(loginId);
